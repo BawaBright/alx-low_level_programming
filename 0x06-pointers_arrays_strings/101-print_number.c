dda69219bcb4be6004ab348561ac94853c75770f
BawaBright <bawabright7@gmail.com>
2023-03-01 06:47:44 -0800

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - function that prints an integer
+ * @n: integer to be printed
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+	int n = 98;
+
+	if (n < 0)
+	{
+	_putchar('-');
+	n = -n;
+	}
+	if (n / 10 != 0)
+	{
+	print_number(n / 10);
+	}
+	_putchar(n % 10 + '0');
+}
+
---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *leet -  a function that encodes a string into 1337.
+ * @s: The character to encode the string.
+ * Return: p
+ */
+
+char *leet(char *s)
+{
+	char *p = s;
+	char *leet_chars = "AaEeOoTtLl";
+	char *leet_nums = "44330771";
+	int i, j;
+
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+	for (j = 0; j < 10; j++)
+	{
+	if (*(s + i) == *(leet_chars + j))
+	{
+	*(p + i) = *(leet_nums + j);
+	break;
+	}
+	}
+	}
+	return (p);
+}
