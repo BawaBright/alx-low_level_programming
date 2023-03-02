89d538ba5c689d6c0bb6d71d2405ae65d4acde8a
BawaBright <bawabright7@gmail.com>
2023-03-02 02:18:50 -0800

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,27 +2,25 @@
 
 /**
  * *leet -  a function that encodes a string into 1337.
- * @s: The character to encode the string.
- * Return: A string to be encoded
+ * @n: The character to encode the string.
+ * Return: n value
  */
 
-char *leet(char *s)
+char *leet(char *n)
 {
-	char *p = s;
-	char *leet_chars = "AaEeOoTtLl";
-	char *leet_nums = "44330771";
+	char s1[] = "AaEeOoTtLl";
+	char s2[] = "4433007711";
 	int i, j;
 
-	for (i = 0; *(s + i) != '\0'; i++)
+	for (i = 0; n[i] != '\0'; i++)
 	{
-	for (j = 0; j < 10; j++)
-	{
-	if (*(s + i) == *(leet_chars + j))
-	{
-	*(p + i) = *(leet_nums + j);
-	break;
+		for (j = 0; j < 10; j++)
+		{
+			if (n[i] == s1[j])
+			{
+				n[j] = s2[j];
+			}
+		}
 	}
-	}
-	}
-	return (p);
+	return (n);
 }
