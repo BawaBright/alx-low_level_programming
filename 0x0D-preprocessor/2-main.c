5b51f3248891173408e74416e81056a865ddbcf1
BawaBright <bawabright7@gmail.com>
2023-03-20 08:32:44 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,10 +2,10 @@
 
 /**
  * main - prints the name of the file
- * @ain -  program that prints the name of the file it was compiled from
+ *
  * Return: Always 0 (Success)
  */
-int ain(void)
+int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
